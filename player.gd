4e8d740a49574cf8eace338cd9ffe264bb18dd2e
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-02-18 23:56:20 +0800

    screen wrapping and other physics shit

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 enum {INIT, ALIVE, INVULNERABLE, DEAD} # values come from their index in the whatever type of iterable this is
 var state = INIT
+var screensize = Vector2.ZERO
 
 @export var engine_power = 500
 @export var spin_power = 8000
@@ -13,18 +14,19 @@ var rotation_dir = 0
 
 func _ready():
 	change_state(ALIVE)
+	screensize = get_viewport_rect().size
 	
 
 func change_state(new_state):
 	match new_state:
 		INIT:
-			$CollisionShape2D.set_deffered("disabled", true)
+			$CollisionShape2D.set_deferred("disabled", true)
 		ALIVE:
-			$CollisionShape2D.set_deffered("disabled", false)
+			$CollisionShape2D.set_deferred("disabled", false)
 		INVULNERABLE:
-			$CollisionShape2D.set_deffered("disabled", true)
+			$CollisionShape2D.set_deferred("disabled", true)
 		DEAD:
-			$CollisionShape2D.set_deffered("disabled", true)
+			$CollisionShape2D.set_deferred("disabled", true)
 	state = new_state
 
 
@@ -37,6 +39,21 @@ func get_input():
 	if state in [DEAD, INIT]:
 		return
 	if Input.is_action_pressed("thrust"):
+		# scalar multiplication, you "stretch" the vector to make it move a direction
 		thrust = transform.x*engine_power
 	# I'm guessing this depends on whether or not it's a negative or positive rotation
 	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+
+
+# again, it's really just about learning what the nodes do; this is a rigid body specific method
+func _physics_process(_delta):
+	constant_force = thrust
+	constant_torque = rotation_dir*spin_power
+	
+	
+# have to use this function when changing a physics body's position or other like "God" shit
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
