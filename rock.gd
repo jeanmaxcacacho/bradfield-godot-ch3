0b4b2bee27e832cb192da62974fff0d2d0be6967
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-02-19 01:43:16 +0800

    complicated start function

--- rock.gd
+++ rock.gd
@@ -4,3 +4,17 @@ var screensize = Vector2.ZERO
 var size
 var radius
 var scale_factor = 0.2
+
+
+# I'm guessing this is the asteroid's constructor
+func start(_position, _velocity, _size):
+	position = _position
+	size = _size
+	mass = 1.5*size
+	$Sprite2D.scale = Vector2.ONE*scale_factor*size
+	radius = int($Sprite2D.texture.get_size().x/2 * $Sprite2D.scale.x)
+	var shape = CircleShape2D.new()
+	shape.radius = radius
+	$CollisionShape2D.shape = shape
+	linear_velocity = _velocity
+	angular_velocity = randf_range(-PI, PI)
