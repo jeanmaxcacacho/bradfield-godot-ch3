25d17d75445263228e0e37bc1ab0d03535797c00
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-02-19 00:12:57 +0800

    tying bullet and player together

--- bullet.gd
+++ bullet.gd
@@ -12,3 +12,10 @@ func start(_transform):
 
 func _process(delta):
 	position += velocity*delta
+
+
+# delete boolets when they're offscreen
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
